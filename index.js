32d5618e5be25a79bc8f06ead41637bece831608
alirezaImani-f4L3e <83541857+alirezaImani-f4L3e@users.noreply.github.com>
2021-05-02 18:40:21 +0430

    test files commited

---
+++ index.js
@@ -0,0 +1,40 @@
+var watch = document.getElementById('demo');
+
+var time = new Date();
+
+watch.innerHTML = `${time.getHours()} : ${time.getMinutes()} : ${time.getSeconds()}`;
+
+setInterval(function() {
+    var time = new Date();
+
+    watch.innerHTML = `${time.getHours()} : ${time.getMinutes()} : ${time.getSeconds()}`;
+
+
+}, 1000);
+
+
+
+function Person(name, family, age, height, wheight) {
+    let first = name[0];
+    first = first.toUpperCase();
+
+    name = `${first}${name.slice(1,name.length)}`
+
+
+    this.name = name;
+    this.family = family;
+    this.age = age;
+    this.height = height;
+
+    this.wheight = wheight;
+    this.BMI = function() {
+        let Bmi = wheight / ((height / 100) * (height / 100));
+        return Bmi;
+    }
+
+
+}
+
+var person1 = new Person("ali", "imani ", 20, 186, 94);
+console.log(person1);
+console.log(person1.BMI());
